9cf3ca1fcd7ee9c767bd3d143c69a481ac3f18b0
yakov cohen <yakovc1431@gmail.com>
2022-02-04 15:34:20 +0200

    #Create router

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const app = express();
 require('dotenv').config();
 const port = 3000;
 
+// routers
+const { searchRouter } = require('./routers/searchRouter');
+
 // middle-ware
 app.use(cors());
 
 // check app
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+// searchRouter
+app.use('/', searchRouter);
+
 // start the server
 app.listen(process.env.PORT || port, () => console.log('Server is running...'));
